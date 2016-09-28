66deec46b96de51d5d61f8944bac7a90f3d76457
electronic Max <max@hip.cat>
2016-09-28 20:23:10 +0100

    quickcommit

--- www/js/main.js
+++ www/js/main.js
@@ -1,20 +1,21 @@
-/* global angular, _, jQuery, Backbone */
+/* global angular, _  */
 
 angular.module('dogma', ['ui.router'])
 	.config(function ($urlRouterProvider) {
 		$urlRouterProvider.otherwise('/main');
 	}).config(function ($stateProvider) {
 		$stateProvider.state('main', {
-			url: '/main',
+			url: '/main?a',
 			templateUrl: 'tmpl/main.html',
 			resolve: {
 				apps:function ($http)  { return $http.get('data/db.json').then(function(x) { return x.data.apps; }); }
 			},
-			controller:function($scope, apps) {
+			controller:function($scope, $stateParams, $anchorScroll, apps) {
 				$scope.apps = apps;
 				$scope.apps.map((a) => {
 					a.platforms = a.platforms.join('; ');
 				});
+				if ($stateParams.a) { $anchorScroll($stateParams.a); }
 				window._s = $scope;
 			}
 		});
@@ -25,24 +26,16 @@ angular.module('dogma', ['ui.router'])
 				apps:function ($http)  { return $http.get('data/db.json').then(function(x) { return x.data.apps; }); }
 			},
 			controller:function($scope, apps) {	
-				$scope.apps = apps;
+				$scope.apps = _(apps).sortBy(function(x) { return x.category + "-" + x.name; }).value();
+				var lastCat;
 				$scope.apps.map((a) => {
 					a.platforms = a.platforms.join('; ');
+					if (lastCat !== a.category) { 
+						a.section = a.category; 
+						lastCat = a.category;
+					}
 				});
-
-                             $scope.medscats = _.chain($scope.meds).sortBy(function (med) {
-                                // console.log('sortby ', med.lcatlabel + ':' + med.lname)
-                                return med.category + ':' + med.lname;
-                            }).map(function (med) {
-                                var section = med.catlabel;
-                                if (section !== lastSection) {
-                                    lastSection = section;
-                                } else {
-                                    section = '';
-                                }
-                                return _.extend({ section: section }, med);
-                            }).value();
-				
+				window._s = $scope;
 			}
 		});
 	}).controller('main', function() { 
